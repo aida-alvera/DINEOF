f286a9ce5c7faa964a9709ac72fc3d042ce7442e
Aida Alvera Azcarate <aida.alvera@gmail.com>
2017-12-08 22:33:48 +0100

    clean up some comments; update compilation

---
+++ README.md
@@ -0,0 +1,25 @@
+# DINEOF
+DINEOF is an EOF-based method to fill in missing data from geophysical fields, such as clouds in sea surface temperature. 
+
+For more information on how DINEOF works, please refer to [Alvera-Azcarate et al (2005)](http://hdl.handle.net/2268/4296) and [Beckers and Rixen (2003)](http://hdl.handle.net/2268/4291). The multivariate application of DINEOF is explained in [Alvera-Azcarate et al (2007)](http://hdl.handle.net/2268/9485), and in [Beckers et al (2006)](http://www.ocean-sci.net/2/183/2006/os-2-183-2006.pdf) the error calculation using an optimal interpolation approach is explained. If you need a copy of any of these papers, don't hesitate to contact us! For more information about the Lanczos solver, see [Toumazou and Cretaux (2001)](https://doi.org/10.1175/1520-0493(2001)129%3C1243:UALEIT%3E2.0.CO;2). 
+
+ 
+# Help pages
+Help pages on how to install and compile DINEOF can be found [here](http://modb.oce.ulg.ac.be/DINEOF).
+
+
+# Installation
+## Binaries
+If you want to use the binaries provided, you can find them at: 
+
+## Installation from source
+Please follow instruction on the [help pages](http://modb.oce.ulg.ac.be/DINEOF), taking into account that you need to install gfortran, make, Arpack and Netcdf as follows (for Ubuntu and Debian):
+
+```bash
+sudo apt-get install gfortran make libarpack2-dev libnetcdf-dev 
+```
+For Ubuntu 16.04 or later you also need:
+
+```bash
+sudo apt-get install libnetcdff-dev 
+```
---
+++ config.mk
@@ -0,0 +1,117 @@
+#==========================================================================
+#
+# Implemented combinations of OS and FORT are:
+#
+#  +--------+-----------+
+#  | OS     | FORT      |
+#  |--------+-----------+
+#  | Linux  | ifort     |
+#  | Linux  | pgi       |
+#  | Linux  | g95       |
+#  | Linux  | gfortran  |
+#  | IRIX   | f90       |
+#  +--------+-----------+
+#
+# For ports to other achictectures, create a corresponding file
+# in the "Compilers" directory. Contributions are most welcome!.
+#
+#==========================================================================
+
+#-------------------------------#
+#  Platform specific variables  #
+#-------------------------------#
+
+OS ?= Linux
+
+# Compiler (gfortran (default), ifort, pgi, g95)
+
+FORT ?= gfortran
+
+#==========================================================================
+#  Machine format, can be either big_endian, little_endian or native machine
+#  format. YOUR CHOICE MUST BE CONSITENT WITH THE WAY YOU CREATE THE INPUT
+#  FILE (see also matlab routines gread.m and gwrite.m)
+#
+#  Note: with some compilers (e.g. IRIX-f90) some options are
+#  not available
+#==========================================================================
+
+FORMAT ?= big_endian
+
+#==========================================================================
+# PRECISION attributed for the internal data processing 
+# single or double (default)
+#==========================================================================
+
+PRECISION ?= double
+
+#==========================================================================
+# Start of user-defined options. Modify macro variables: 'on' is TRUE while
+# blank is FALSE.
+#==========================================================================
+#
+#  Activate debugging compiler options:
+
+DEBUG ?=
+
+#  Activate static linking
+
+STATIC ?= 
+
+
+#==========================================================================
+# Library names and locations
+# Variables name use the following naming scheme:
+# 
+# <library>_INCDIR: directory containing the include files and compiled modules (e.g. /home/user/mpi/include)
+# <library>_LIBDIR: directory containing the library  (e.g. /home/user/mpi/lib)
+# <library>_LIB: name of the library (including dependencies) for the linker (e.g. -lmpi -lpthread)
+#
+#==========================================================================
+
+#==========================================================================
+# netCDF library
+# Note if the nc-config tool is present and NETCDF_CONFIG is defined, then the values of 
+# NETCDF_INCDIR, NETCDF_LIBDIR and NETCDF_LIB are not used
+#==========================================================================
+
+#NETCDF_CONFIG ?= nf-config
+
+#==========================================================================
+# This directory should contains a file usually called netcdf.inc and 
+# netcdf.mod
+#==========================================================================
+
+#NETCDF_INCDIR ?= 
+
+#==========================================================================
+# Locations of the NetCDF library
+# This directory should contains a file usually called libnetcdf.a
+#==========================================================================
+
+#NETCDF_LIBDIR ?= 
+#NETCDF_LIB ?= -lnetcdf
+
+#==========================================================================
+# Lapack library
+#==========================================================================
+
+#LAPACK_LIBDIR ?= 
+#LAPACK_LIB ?= -llapack
+
+#==========================================================================
+# BLAS library
+#==========================================================================
+
+#BLAS_LIBDIR ?=
+#BLAS_LIB ?= -lblas
+
+#==========================================================================
+# ARPACK library
+# Locations of the ARPACK library
+# This directory should contains a file usually called libarpack.a
+#==========================================================================
+
+#ARPACK_LIBDIR ?=
+#ARPACK_LIB ?= -larpack
+
